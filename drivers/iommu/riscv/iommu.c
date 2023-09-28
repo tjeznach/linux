fcf4680b9e2dc5b3eb8a07c2b5f05f57e7c5daef
Tomasz Jeznach <tjeznach@rivosinc.com>
2023-09-27 17:22:11 -0700

    NEXT: iommu/riscv: Force pci_request_acs() at arch init
    ACS should be enabled before IOMMU probe_device calls.
    
    If IOMMU hardware is implemented as PCIe integrated endpoint it
    is discovered as part of PCIe bus enumeration. However, ACS is
    not yet enabled for the root complex before discovery of iommu
    devices done by DT probes (based on iommu-map root complex property).
    
    We will force ACS enable during architecture init sequence, on systems
    with riscv-iommu driver enabled.
    
    Signed-off-by: Tomasz Jeznach <tjeznach@rivosinc.com>

--- drivers/iommu/riscv/iommu.c
+++ drivers/iommu/riscv/iommu.c
@@ -1742,3 +1742,12 @@ err_queue_disable:
 	riscv_iommu_queue_disable(&iommu->cmdq);
 	return rc;
 }
+
+static __init int riscv_iommu_request_acs(void)
+{
+	pci_request_acs();
+
+	return 0;
+}
+
+arch_initcall(riscv_iommu_request_acs);
