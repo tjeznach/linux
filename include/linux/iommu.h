0c37c0e802ae988744d988fa53d0c87c742bc682
Lu Baolu <baolu.lu@linux.intel.com>
2024-10-09 12:11:47 +0800

    iommu: Remove iommu_domain_alloc()
    The iommu_domain_alloc() interface is no longer used in the tree anymore.
    Remove it to avoid dead code.
    
    There is increasing demand for supporting multiple IOMMU drivers, and this
    is the last bus-based thing standing in the way of that.
    
    Signed-off-by: Lu Baolu <baolu.lu@linux.intel.com>
    Reviewed-by: Jason Gunthorpe <jgg@nvidia.com>

--- include/linux/iommu.h
+++ include/linux/iommu.h
@@ -788,7 +788,6 @@ extern int bus_iommu_probe(const struct bus_type *bus);
 extern bool iommu_present(const struct bus_type *bus);
 extern bool device_iommu_capable(struct device *dev, enum iommu_cap cap);
 extern bool iommu_group_has_isolated_msi(struct iommu_group *group);
-extern struct iommu_domain *iommu_domain_alloc(const struct bus_type *bus);
 struct iommu_domain *iommu_paging_domain_alloc(struct device *dev);
 extern void iommu_domain_free(struct iommu_domain *domain);
 extern int iommu_attach_device(struct iommu_domain *domain,
@@ -1089,11 +1088,6 @@ static inline bool iommu_present(const struct bus_type *bus)
 static inline bool device_iommu_capable(struct device *dev, enum iommu_cap cap)
 {
 	return false;
-}
-
-static inline struct iommu_domain *iommu_domain_alloc(const struct bus_type *bus)
-{
-	return NULL;
 }
 
 static inline struct iommu_domain *iommu_paging_domain_alloc(struct device *dev)
